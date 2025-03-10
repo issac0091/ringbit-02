754ab2d1e5e63f94fca1bbed9b774b44e72016cd
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-03-10 20:17:27 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onGesture(Gesture.ScreenDown, function () {
+    RingbitCar.back()
+})
+input.onGesture(Gesture.EightG, function () {
+    RingbitCar.brake()
+})
+input.onButtonPressed(Button.A, function () {
+    RingbitCar.turnright()
+})
+input.onButtonPressed(Button.AB, function () {
+    RingbitCar.forward()
+})
+input.onButtonPressed(Button.B, function () {
+    RingbitCar.turnleft()
+})
+RingbitCar.init_wheel(AnalogPin.P0, AnalogPin.P1)
+RingbitCar.forward()
